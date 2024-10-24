167fab43fe1d1c47ae30c65a5eee5f1e22ca63f6
Łukasz Domin <ld123631@stud.ur.edu.pl>
2024-10-24 23:14:34 +0200

    Naprawa diagonalnego ruchu i reduckcja jittering pixels podczas ruchu

--- Player/Scripts/le_p_layer.gd
+++ Player/Scripts/le_p_layer.gd
@@ -11,26 +11,28 @@ var state: States = States.IDLE
 var direction : Vector2 = Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
-	var horizontal_d = Input.get_axis("left", "right")
-	velocity.x = horizontal_d * SPEED
+	var horizontal_d = Input.get_action_strength("right") - Input.get_action_strength("left")
+	var vertical_d = Input.get_action_strength("down") - Input.get_action_strength("up")
 	
-	var vertical_d = Input.get_axis("down", "up")
-	velocity.y = -vertical_d * SPEED
-
-	velocity.normalized()
-
+	var input_direction: = Vector2(horizontal_d, vertical_d)
+		
+	input_direction = input_direction.normalized()
+	
+	velocity= input_direction * SPEED
 	move_and_slide()
+	
+
 	UpdateAnimation(horizontal_d, vertical_d)
 
 func UpdateAnimation(horizontal_d, vertical_d = 0) -> void:
 	if vertical_d != 0:
 		if vertical_d == -1:
-			animated_sprite.play("down")
-		else:
 			animated_sprite.play("up")
+		else:
+			animated_sprite.play("down")
 	elif horizontal_d != 0:
 		#animated_sprite.flip_h = (horizontal_d == -1)
-		if(horizontal_d == -1):
+		if horizontal_d == -1:
 			animated_sprite.play("left")
 		else: 
 			animated_sprite.play("right")
